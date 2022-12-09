40343b8679cd9762fbe5794cce741a687a0f65b8
Dileep Ratnayake <dileep_ratnayake@hotmail.com>
2022-12-10 08:52:35 +1300

    create stock movement model and migration

---
+++ database/migrations/2022_12_09_192451_create_stock_movements_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock_movements', function (Blueprint $table) {
+            $table->id();
+            $table->date('movementDate');
+            $table->enum('type', \App\Utils\Settings::ALLOWED_TYPES);
+            $table->unsignedInteger('quantity');
+            $table->unsignedDouble('unitPrice')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock_movements');
+    }
+};
