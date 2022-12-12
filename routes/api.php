31af8335199e380e138a62388d1ba773c99d4d54
Dileep Ratnayake <dileep_ratnayake@hotmail.com>
2022-12-13 10:49:24 +1300

    add name to route and update welcome.blade for UI

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/checkAvailableQuantity', [StockController::class, 'checkAvailableQuantity']);
+Route::post('/checkAvailableQuantity', [StockController::class, 'checkAvailableQuantity'])->name('checkQuantity');
