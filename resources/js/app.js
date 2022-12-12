3ea058ab5104d2cb65257de644abea1e7afe9807
Dileep Ratnayake <dileep_ratnayake@hotmail.com>
2022-12-13 11:57:43 +1300

    set up vue with vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+import { createApp } from "vue";
+
+import quantityChecker from './components/quantity-checker.vue';
+
+createApp(quantityChecker).mount("#app");
---
+++ resources/js/components/quantity-checker.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h3>Quantity Checker</h3>
+    </div>
+</template>
+
+<script>
+import axios, {isCancel, AxiosError} from 'axios';
+
+export default {
+    name: "quantityChecker.vue",
+    data() {
+        return {
+            'quantity' : 0,
+            'isAvailable' : false,
+            'cost' : 0,
+            'totalAvailable' : 0
+        }
+    },
+    methods: {
+        checkQuantity(){
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
