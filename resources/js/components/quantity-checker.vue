04b16dd10258050d8014e0a672d12751f9924648
Dileep Ratnayake <dileep_ratnayake@hotmail.com>
2022-12-13 13:34:54 +1300

    remove axios add tailwind and update vue component

--- resources/js/components/quantity-checker.vue
+++ resources/js/components/quantity-checker.vue
@@ -1,25 +1,58 @@
 <template>
-    <div>
-        <h3>Quantity Checker</h3>
+    <div class="overflow-hidden border-t border-l border-r border-gray-400 bg-gray-200 p-8">
+        <div class="group p-6 bg-white max-w-screen w-3/5 shadow-lg overflow-hidden mx-auto h-48">
+            <form @submit="onSubmit">
+                <p class="font-semibold text-lg mb-1 text-gray-900">Check Stock Availability</p>
+                <input class="px-4 py-2 leading-tight border border-blue" type="number" v-model="quantity">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-1" type="submit">Check</button>
+            </form>
+        </div>
+        <div class="group p-6 bg-white max-w-screen w-3/5 shadow-lg overflow-hidden mx-auto h-48" v-if="hasResponse">
+            <label v-if="response.isAvailable">Cost : {{ response.cost }}</label>
+            <label v-if="response.isAvailable==false">Required quantity unavailable. Max available units : {{ response.totalAvailable }}</label>
+
+        </div>
     </div>
 </template>
 
 <script>
-import axios, {isCancel, AxiosError} from 'axios';
-
 export default {
     name: "quantityChecker.vue",
     data() {
         return {
             'quantity' : 0,
-            'isAvailable' : false,
-            'cost' : 0,
-            'totalAvailable' : 0
+            'hasResponse' : false,
+            'response' : {
+                'isAvailable' : false,
+                'cost' : 0,
+                'totalAvailable' : 0
+            }
         }
     },
     methods: {
-        checkQuantity(){
-
+        onSubmit(e){
+            e.preventDefault();
+            this.hasResponse = false;
+            var self = this;
+            axios.post('/api/checkAvailableQuantity',
+        {
+                quantity: this.quantity
+            },{
+                'headers' : {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                }
+            })
+            .then(function (response) {
+                self.hasResponse = true;
+                self.response = response.data;
+            })
+            .catch(function (error) {
+                alert(error.response.data.message)
+            })
+            .finally(function () {
+                // always executed
+            });
         }
     }
 }
